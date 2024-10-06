59462177a00d60a298e47408295e73d8f482f1a7
Derek Kraan <derek@kraan.dev>
2024-10-06 15:49:11 +0200

    Support back to 1.14?

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HordePro.MixProject do
     [
       app: :horde_pro,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
