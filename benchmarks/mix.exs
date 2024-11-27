c19eeebe24adc9fd7c66a7f56ae3f5c6c53f109a
Derek Kraan <derek@kraan.dev>
2024-11-27 09:59:52 +0100

    tweak mixfile

--- benchmarks/mix.exs
+++ benchmarks/mix.exs
@@ -23,7 +23,7 @@ defmodule HordeBench.MixProject do
     [
       {:benchee, "> 0.0.0"},
       {:horde, "> 0.0.0"},
-      {:horde_pro, path: "~/Code/horde_pro"},
+      {:horde_pro, path: ".."},
       {:sql_fmt, "> 0.0.0"}
     ]
   end
